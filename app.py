9baec20e2d4eaca7b724bec338c66b841d19b33f
nathan <nyee4@asu.edu>
2024-12-03 21:14:48 -0700

    added page navigation and login functionality

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, url_for, render_template, redirect, session
+import psycopg2
+
+app = Flask(__name__)
+
+app.secret_key = "0fba08b30f04cf531dabf1a56f35aab98091bfd3930c757573dbc02c152f23d8"
+
+conn = psycopg2.connect(database="airline",
+host="localhost",
+user="postgres",
+password="password",
+port=5432)
+
+cursor = conn.cursor()
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    msg = ""
+    if "userid" in session:
+        return redirect(url_for("home"))
+    elif request.method == "POST":
+            email = request.form.get("email")
+            password = request.form.get("password")
+            cursor.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email, password,))
+            user = cursor.fetchone()
+            if user:
+                session["userid"] = user[0]
+                session["name"] = user[1]
+                return redirect(url_for("home"))
+            else:
+                msg = "Invalid login"
+    return render_template("index.html", msg=msg)
+
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    if "userid" not in session:
+        return redirect(url_for("index"))
+    return render_template("homeplaceholder.html") # TODO: change to home.html
+
+@app.route("/booking", methods=["GET", "POST"])
+def booking():
+    if "userid" not in session:
+        return redirect(url_for("index"))
+    return render_template("bookingplaceholder.html") # TODO: change to booking.html
+
+@app.route("/logout")
+def logout():
+    if "userid" not in session:
+        return redirect(url_for("index"))
+    session.pop("userid", None)
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run()
